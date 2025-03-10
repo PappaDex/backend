735dd1297ce2428f36162c3cb6fc8538e0cb3650
Karlo <karlobedu@gmail.com>
2025-03-10 21:45:45 +0100

    brisanje fronta s backenda

--- projekt/Frontend/src/services/HttpService.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import { BACKEND_LOKALNO } from "../constants";
-
-export const HttpService = axios.create({
-  baseURL: BACKEND_LOKALNO,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
